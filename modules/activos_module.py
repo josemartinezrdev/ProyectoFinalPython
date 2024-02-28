4e1b2566a301538ddc4ca663015687f7c5309210
dannagalvarezr0918 <158192981+dannagalvarezr0918@users.noreply.github.com>
2024-02-28 07:31:30 -0500

    28/02/24 DG: ahora si zonas

--- modules/activos_module.py
+++ modules/activos_module.py
@@ -96,25 +96,4 @@ def delete_activos():
 def search_activos():
     pass
 
-def add_zonas():
-
-    inventario = cf.read_file(cf.BASE+'inventario.json')
-
-    nro_zona = input('Ingrese el número de la zona: ')
-    clear_screen()
-    nombre_zona = input('Ingrese el nombre de la zona: ')
-    clear_screen()
-    capacidad_zona = input('Ingrese la capacidad de la zona')
-    clear_screen()
-
-    zona = {
-        'nro_zona': nro_zona,
-        'nombre_zona': nombre_zona,
-        'capacidad_zona': capacidad_zona
-    }
-
-    inventario.get('activos').update({nro_zona:zona})
-    cf.update_file('inventario.json', inventario)
-
-    return True
 
